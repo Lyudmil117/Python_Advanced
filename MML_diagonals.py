02f50694bfb4ab36819bad7eb1c0f7b94b25bfb5
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2023-02-02 08:55:53 +0200

    Update MML_diagonals.py

--- MML_diagonals.py
+++ MML_diagonals.py
@@ -7,3 +7,4 @@ second_diagonal = [matrix[r][n - r - 1] for r in range(n - 1, -1, -1)]
 
 print(f'Primary diagonal: {", ".join(str(x) for x in first_diagonal)}. Sum: {sum(first_diagonal)}')
 print(f'Secondary diagonal: {" ,".join(str(x) for x in second_diagonal)[::-1]}. Sum: {sum(second_diagonal)}')
+
